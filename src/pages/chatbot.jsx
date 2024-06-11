952982af0b7eca5b0505eb39ccb1fb04badad67f
Hernando Lopez <106278414+nandolopez7@users.noreply.github.com>
2024-06-11 01:23:39 -0500

    Update chatbot.jsx

--- src/pages/chatbot.jsx
+++ src/pages/chatbot.jsx
@@ -49,7 +49,7 @@ export function ChatBot() {
   // eslint-disable-next-line
   const [visibleEvents, setVisibleEvents] = useState([]);
   const [eventsback, setEvents] = useState([]);
-  const URL_BACKEND = "https://tickethub-back.onrender.com/";
+  const URL_BACKEND = "https://tickethub-back.onrender.com";
 
   const handlePrint = () => {
     window.print();
